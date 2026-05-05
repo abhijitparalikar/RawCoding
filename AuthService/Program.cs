d35fdb46e17e0138834066a33e417dbea019f3f9
Abhijit Paralikar <abhijit@propertycontrol.com>
2026-05-05 17:17:22 -0400

    Add built-in authentation mechanism

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
@@ -12,9 +13,17 @@ namespace AuthBasics
             builder.Services.AddDataProtection()
                 .SetApplicationName("AuthBasics");
 
+            //built-in; add cookie authentication for this example
+            builder.Services.AddAuthentication("cookie")
+                .AddCookie("cookie");
+
             var app = builder.Build();
 
-            app.Use(async (ctx, next) =>
+            //built-in; add middleware for authentication
+            app.UseAuthentication();
+
+            #region custom authentication layer
+            /*app.Use(async (ctx, next) =>
             {
                 if (ctx.Request.Path == "/user")
                 {
@@ -37,18 +46,35 @@ namespace AuthBasics
                 await next();
             });
 
+            app.MapGet("/login", (HttpContext ctx, IDataProtectionProvider idp) =>
+           {
+               var protector = idp.CreateProtector("auth-cookie");
+               ctx.Response.Cookies.Append("auth",$"{protector.Protect("usr:abhi")}");
+               return "Login Successful";
+           });*/
+            #endregion
+
+
             app.MapGet("/", () => "Hello World!");
 
             app.MapGet("/user", (HttpContext ctx, IDataProtectionProvider idp) =>
             {
+                //retrieval can remain the same regardless of built-in or custom implementation
                 return ctx.User.FindFirst("user")?.Value ?? "Authentication Failure; No user found";
             });
 
-            app.MapGet("/login", (HttpContext ctx, IDataProtectionProvider idp) =>
+           
+
+            app.MapGet("/login", async (HttpContext ctx) =>
             {
-                var protector = idp.CreateProtector("auth-cookie");
-                ctx.Response.Cookies.Append("auth",$"{protector.Protect("usr:abhi")}");
-                return "Login Successful";
+                List<Claim> claims = [new Claim("user", "abhi")];
+                ClaimsIdentity identity = new(claims, "cookie");
+                ClaimsPrincipal principal = new(identity);
+                ctx.User = principal;
+
+                //built-in; use built in SignInAsync to deal cookie
+                await ctx.SignInAsync(principal);
+                return "login success";
             });
 
             app.Run();
